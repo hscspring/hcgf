8f9b1843d2c89aeff60a4447bd341116096f777d
hscspring <haoshaochun@gmail.com>
2023-03-25 22:00:02 +0800

    remove demo

--- demo.py
+++
@@ -1,7 +0,0 @@
-from hcgf.sft import GlmLora
-
-data_path = "./chatgpt_finetune_faq.json"
-model_id = "THUDM/chatglm-6b"
-device = "cuda:1"
-model = GlmLora(model_id, device=device)
-model.load_data(data_path).tune(device)
